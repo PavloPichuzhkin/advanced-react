76efeba5ff2314a62290a03c1fd3693368c99c74
PavloPichuzhkin <pichujkin1986@gmail.com>
2023-07-09 00:38:19 +0300

    fix line separators

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,6 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
         'max-len': ['error', { comments: 160, code:150 }],
-
     },
     globals: {
         __IS_DEV__: true,
