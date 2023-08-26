f5f9d8e210c0291c5f70409debfae286a6282b95
PavloPichuzhkin <pichujkin1986@gmail.com>
2023-08-26 14:56:30 +0300

    api interceptors

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,11 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 // const baseURL = __IS_DEV__ ? 'http://localhost:8000' : 'https://production...';
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+    }
+    return config;
 });
