45c3ee3a887c8be987daa6dc3c5cf6da8145525c
PavloPichuzhkin <pichujkin1986@gmail.com>
2023-11-05 02:22:51 +0200

    PageLoader redesign

--- src/widgets/PageLoader/ui/PageLoader.tsx
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -2,18 +2,34 @@ import { HTMLAttributes } from 'react';
 import { classNames } from '@/shared/lib/helpers/classNames/classNames';
 import { Loader } from '@/shared/ui/deprecated/Loader';
 import cls from './PageLoader.module.scss';
+import { ToggleFeatures } from '@/shared/lib/features';
 
 interface PageLoaderProps extends HTMLAttributes<HTMLDivElement> {
     className?: string;
 }
 
 export const PageLoader = ({ className, ...otherProps }: PageLoaderProps) => (
-    <div
-        className={classNames(cls.PageLoader, {}, [className])}
-        {...otherProps}
-    >
-        <div className={cls.beneathPageLoader}>
-            <Loader />
-        </div>
-    </div>
+    <ToggleFeatures
+        feature='isAppRedesigned'
+        on={
+            <div
+                className={classNames(cls.PageLoaderRedesigned, {}, [
+                    className,
+                ])}
+                {...otherProps}
+            >
+                <Loader />
+            </div>
+        }
+        off={
+            <div
+                className={classNames(cls.PageLoader, {}, [className])}
+                {...otherProps}
+            >
+                <div className={cls.beneathPageLoader}>
+                    <Loader />
+                </div>
+            </div>
+        }
+    />
 );
